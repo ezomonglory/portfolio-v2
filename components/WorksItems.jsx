b26d3484e92119e78880c96f231d2580700a9161
Ezomon Glory <ezomonglory01@gmail.com>
2023-10-09 07:33:46 +0100

    project name change

--- components/WorksItems.jsx
+++ components/WorksItems.jsx
@@ -14,7 +14,7 @@ const WorksItems = ({work, i}) => {
      <div className={`${i === 4 ? "flex justify-between border-transparent border-[0.5px] border-y-[#9F9F9F] pt-[24px] pb-[60px] items-center" : "flex justify-between border-transparent border-[0.5px] border-t-[#9F9F9F] items-center pt-[24px]"}`}>
         <div className='flex space-x-[24px] md:space-x-[64px] items-center'>
             <p className='md:text-[20px] text-[12px] text-black neuereg'>{work.number}</p>
-            <h1 className='text-[#606060] md:text-[40px] text-[20px] neuereg md:leading-[48px]'>{work.name}</h1>
+            <h1 className='text-[#606060] md:text-[40px] text-[20px] neuereg md:leading-[48px] tracking-[-1px] '>{work.name}</h1>
         </div>
 
         <Image src='/Frame.svg' alt='arrow' width={56} height={56} className='hidden md:block'/>
